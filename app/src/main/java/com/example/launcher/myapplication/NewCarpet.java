66277898cff6f8b645dd68b69d5487a9c4f275de
Amir Muhammad <amk9978@gmail.com>
2019-05-30 10:23:53 +0430

    A little name changing

--- app/src/main/java/com/example/launcher/myapplication/NewCarpet.java
+++ app/src/main/java/com/example/launcher/myapplication/NewCarpet.java
@@ -1,17 +1,14 @@
 package com.example.launcher.myapplication;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 
 public class NewCarpet extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_new_carpet);
+        setContentView(R.layout.activity_seller);
 
     }
 }
